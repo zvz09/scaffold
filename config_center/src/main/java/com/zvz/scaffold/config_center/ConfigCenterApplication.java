29cf608e507af9a9bb86f2a24f07a3aeee03e3eb
zvz <18237119598@163.com>
2019-10-17 07:17:16 +0800

    config  改造

--- config_center/src/main/java/com/zvz/scaffold/config_center/ConfigCenterApplication.java
+++ config_center/src/main/java/com/zvz/scaffold/config_center/ConfigCenterApplication.java
@@ -3,9 +3,12 @@ package com.zvz.scaffold.config_center;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigCenterApplication {
 
     public static void main(String[] args) {
