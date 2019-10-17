6759fa02de5a0611aa9655e390ea3d41978104c8
zvz <18237119598@163.com>
2019-10-17 16:05:31 +0800

    admin

---
+++ service-admin/src/main/java/com/zvz/scaffold/serviceadmin/ServiceAdminApplication.java
@@ -0,0 +1,14 @@
+package com.zvz.scaffold.serviceadmin;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class ServiceAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAdminApplication.class, args);
+    }
+}
