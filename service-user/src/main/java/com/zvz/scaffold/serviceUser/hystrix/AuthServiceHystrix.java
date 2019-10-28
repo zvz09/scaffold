7d028a4e958c7a30fa1281bd66afe8d357bf0da1
zvz <18237119598@163.com>
2019-10-28 08:02:35 +0800

    OAth2-jwt

--- service-user/src/main/java/com/zvz/scaffold/serviceUser/hystrix/AuthServiceHystrix.java
+++ service-user/src/main/java/com/zvz/scaffold/serviceUser/hystrix/AuthServiceHystrix.java
@@ -2,12 +2,18 @@ package com.zvz.scaffold.serviceUser.hystrix;
 
 import com.zvz.scaffold.serviceUser.javaBean.JWT;
 import com.zvz.scaffold.serviceUser.service.AuthServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthServiceHystrix implements AuthServiceClient {
+
+    private static Logger log = LoggerFactory.getLogger(AuthServiceHystrix.class);
+
     @Override
     public JWT getToken(String authorization, String type, String username, String password) {
+        log.error("获取token失败");
         return null;
     }
 }
